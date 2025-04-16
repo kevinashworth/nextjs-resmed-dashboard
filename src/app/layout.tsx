be07b439a34e92348d2bd035ff6fd1d3f441478c
Kevin Ashworth <kevinashworth@yahoo.com>
2025-04-15 17:29:33 -0700

    Remove unused nav, redirect to sleep-data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,5 @@
 import { cx } from "class-variance-authority";
 import { Geist, Geist_Mono } from "next/font/google";
-import Link from "next/link";
-
-// import { getThemeData, getThemeDataFromCookies } from "@/components/themePicker/ThemePicker";
 
 import type { Metadata } from "next";
 
@@ -29,29 +26,11 @@ export default async function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  // const { darkMode, hue } = await getThemeDataFromCookies();
-  // const { className, style } = getThemeData(hue!, darkMode === "yes");
-  // console.log({ className, style });
-
   return (
     <html lang="en">
       <body className={cx(geistSans.variable, geistMono.variable, "antialiased")}>
-        <nav className="mx-auto flex justify-center gap-4 bg-orange-50 text-xs font-medium text-orange-800">
-          <Link className="m-1 p-1 hover:bg-orange-100 hover:underline" href="/">
-            Home
-          </Link>
-          <Link className="m-1 p-1 hover:bg-orange-100 hover:underline" href="/sleep-data">
-            Sleep Data
-          </Link>
-          {/* <Link className="m-1 p-1 hover:bg-orange-100 hover:underline" href="/settings">
-            Settings
-          </Link> */}
-        </nav>
-
         <main className="m-1 p-1">{children}</main>
       </body>
     </html>
   );
 }
-
-// {/* <html lang="en" className={cx("h-full", { className: !!className })} style={style}> */}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,40 +1,8 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
-      <main className="row-start-2 flex flex-col items-center gap-8 sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-          style={{ width: "auto" }}
-        />
-        <ol className="list-inside list-decimal text-center font-[family-name:var(--font-geist-mono)] text-sm sm:text-left">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="rounded bg-orange-50/[.05] px-1 py-0.5 font-[family-name:var(--font-geist-mono)] font-semibold text-orange-500 dark:bg-white/[.06]">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-      </main>
-      <footer className="row-start-3 flex flex-wrap items-center justify-center gap-6">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image aria-hidden src="/globe.svg" alt="Globe icon" width={16} height={16} />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      <main className="row-start-2 flex flex-col items-center gap-8 sm:items-start"></main>
+      <footer className="row-start-3 flex flex-wrap items-center justify-center gap-6"></footer>
     </div>
   );
 }
