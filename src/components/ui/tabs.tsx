9d0cbbfe0d26b98837a726e6c906ca1c0662c458
Kevin Ashworth <kevinashworth@yahoo.com>
2025-12-13 16:54:51 -0800

    Clean up code
    Remove unused, commented-out, dark, rtl

--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -12,15 +12,7 @@ const TabsList = React.forwardRef<
   React.ComponentRef<typeof TabsPrimitive.List>,
   React.ComponentPropsWithoutRef<typeof TabsPrimitive.List>
 >(({ className, ...props }, ref) => (
-  <TabsPrimitive.List
-    ref={ref}
-    className={cn(
-      "-mb-px flex flex-nowrap pb-4 rtl:space-x-reverse",
-      // "inline-flex h-9 items-center justify-center rounded-lg bg-stone-100 p-1 text-stone-500 dark:bg-stone-800 dark:text-stone-400",
-      className,
-    )}
-    {...props}
-  />
+  <TabsPrimitive.List ref={ref} className={cn("-mb-px flex flex-nowrap pb-4", className)} {...props} />
 ));
 TabsList.displayName = TabsPrimitive.List.displayName;
 
@@ -32,11 +24,6 @@ const TabsTrigger = React.forwardRef<
     ref={ref}
     className={cn(
       "inline-block border-b-2 border-gray-50 p-4 text-center text-sm font-medium hover:border-gray-200 disabled:cursor-not-allowed",
-      // "inline-flex items-center justify-center whitespace-nowrap rounded-md px-3 py-1 text-sm font-medium ring-offset-white transition-all",
-      // "focus-visible:outline-hidden focus-visible:ring-2 focus-visible:ring-stone-950 focus-visible:ring-offset-2",
-      // "disabled:pointer-events-none disabled:opacity-50",
-      // "data-[state=active]:bg-white data-[state=active]:text-stone-950 data-[state=active]:shadow-sm",
-      // "dark:ring-offset-stone-950 dark:focus-visible:ring-stone-300 dark:data-[state=active]:bg-stone-950 dark:data-[state=active]:text-stone-50",
       className,
     )}
     {...props}
@@ -47,16 +34,7 @@ TabsTrigger.displayName = TabsPrimitive.Trigger.displayName;
 const TabsContent = React.forwardRef<
   React.ComponentRef<typeof TabsPrimitive.Content>,
   React.ComponentPropsWithoutRef<typeof TabsPrimitive.Content>
->(({ className, ...props }, ref) => (
-  <TabsPrimitive.Content
-    ref={ref}
-    className={cn(
-      "mt-2 ring-offset-white focus-visible:ring-2 focus-visible:ring-stone-950 focus-visible:ring-offset-2 focus-visible:outline-hidden dark:ring-offset-stone-950 dark:focus-visible:ring-stone-300",
-      className,
-    )}
-    {...props}
-  />
-));
+>(({ className, ...props }, ref) => <TabsPrimitive.Content ref={ref} className={cn("mt-2", className)} {...props} />);
 TabsContent.displayName = TabsPrimitive.Content.displayName;
 
 export { Tabs, TabsList, TabsTrigger, TabsContent };
