0d3b9cc665c804cc755f961526420d1bb927f9a7
Kevin Ashworth <1218844+kevinashworth@users.noreply.github.com>
2026-07-07 16:23:14 -0700

    refactor(DisplayJSON): remove unused React import

--- src/components/DisplayJSON.tsx
+++ src/components/DisplayJSON.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 
 import { cx } from "class-variance-authority";
 import { format as prettyFormat } from "pretty-format";
